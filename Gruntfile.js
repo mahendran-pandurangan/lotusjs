fe1b928ff9c1eb1cd3f037a35ee5d6166f641786
MJ <write2mahen@gmail.com>
2016-05-28 10:35:58 -0700

    Create Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        webdriver: { // for use with webdriver.io
+            options: {
+                desiredCapabilities: {
+                    browserName: 'chrome' // No Xvfb required
+                }
+            },
+            chrome: {
+                tests: ['./test/test-webdriver.js'],
+                options: {
+                    desiredCapabilities: {
+                        browserName: 'chrome'
+                    }
+                }
+            },
+        },
+    });
+    grunt.loadNpmTasks('grunt-webdriver');
+    grunt.registerTask('default', ['webdriver']);
+};
