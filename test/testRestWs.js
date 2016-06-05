428507287eb483127dc25da338a85aceb3a720d1
MJ <write2mahen@gmail.com>
2016-06-05 10:53:38 -0700

    Update testRestWs.js

--- test/testRestWs.js
+++ test/testRestWs.js
@@ -4,7 +4,7 @@ var logger = require('../lib/logger');
 
 describe('rest api suite', function(){
     it('rest test', function() {
-        var test = restCall.getRestWebServicesUtil('http://github.com/', 'GET', 'content-type=application/json', '');
+        var test = restCall.getRestWebServicesUtil('https://www.google.com/?gws_rd=ssl', 'GET', 'content-type=application/json', '');
         logger.info(test.statusCode);
         logger.info(test.body);
     });
