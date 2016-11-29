5385b54003d38d68ca5a136ca9704d2eb059124e
MJ <write2mahen@gmail.com>
2016-11-29 12:56:07 -0800

    Update Lotus.js

--- main/Lotus.js
+++ main/Lotus.js
@@ -2,11 +2,13 @@
 var Common = require('../lib/common-util');
 var Data = require('../lib/excel-provider');
 var Logger = require('../lib/logger');
+var Runner = require('main/Runner');
 var Lotus = function(){
     this.ds = {
     readExcelData: function(fileName, sheetName) {
       return Data.readExcelData(fileName, sheetName);
     }
   }
+    this.runner = new Runner(this);
 }
 module.exports = new Lotus();
